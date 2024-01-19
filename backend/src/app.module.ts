b3a712b3662ab5569800dac2c32f60bf16077eaf
Will Kessler <willkessler@gmail.com>
2024-01-19 13:01:14 -0800

    improved PORT parsing

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -15,7 +15,7 @@ import { EZNotification } from './ezNotification/entities/ezNotification.entity'
         TypeOrmModule.forRoot({
             type: 'postgres',
             host:     process.env.DB_HOST,
-            port:     parseInt(process.env.DB_PORT),
+            port:     parseInt(process.env.DB_PORT,10),
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
