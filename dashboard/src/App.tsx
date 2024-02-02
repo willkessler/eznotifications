33fd7a6d70c5b3f75fd03280b68c61ccc67ee6b8
Will Kessler <willkessler@gmail.com>
2024-02-01 16:43:51 -0800

    figured out how to style the login button

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,24 +1,32 @@
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { Anchor, Button, Code, Group, Image, Modal, Textarea, Text } from '@mantine/core';
+import { IconLogout } from '@tabler/icons-react';
 import { Router } from './Router';
 import { theme } from './theme';
 import { UserButton, SignOutButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-react"
+import classes from './pages/NavbarSimple.module.css';
 
 export default function App() {
   return (
     <>
-      <SignedOut>
-        <p>This content is public. Only signed out users can see this.</p>
-        <SignInButton afterSignInUrl="/dashboard" />
-      </SignedOut>
-      <SignedIn>
-        <p>This content is truly private. Only signed in users can see this.</p>
-        <UserButton />
-        <MantineProvider theme={theme}>
-          <Router />
-        </MantineProvider>
-      </SignedIn>
+      <MantineProvider theme={theme}>
+        <SignedOut>
+          <Text>You are signed out.</Text>
+          <Button>
+              <SignInButton afterSignInUrl="/dashboard">
+                <Text>Sign in</Text>
+              </SignInButton>
+          </Button>
+        </SignedOut>
+
+        <SignedIn>
+          <p>This content is truly private. Only signed in users can see this.</p>
+          <UserButton />
+            <Router />
+        </SignedIn>
+      </MantineProvider>
     </>
   );
 }
