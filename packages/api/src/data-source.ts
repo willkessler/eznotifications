c4442c19bf99f681dfd8f6daf78747e720701cee
Will Kessler <willkessler@gmail.com>
2024-02-27 14:33:01 -0800

    moved data-source.ts to use ENV vars

--- packages/api/src/data-source.ts
+++ packages/api/src/data-source.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { EndUser } from './EZNotification/entities/EndUsers.entity';
 import { EndUsersServed } from './EZNotification/entities/EndUsersServed.entity';
@@ -11,12 +12,12 @@ import { EZNotification } from './EZNotification/entities/EZNotification.entity'
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432', 10),
     schema: 'public',
-    username: 'postgres',
-    password: 'postgres',
-    database: 'notifications_dev2',
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_DATABASE || 'notifications_dev2',
     entities: [EZNotification, EndUser, EndUsersServed, Organization, User, UserEmails, ApiKey, PermittedDomains, PricingModel],
     synchronize: false,
     migrations: ["src/migration/*.ts"],
