2269cd99934cf46ef2b64c66a9e14890b6931d10
Will Kessler <willkessler@gmail.com>
2024-02-27 14:41:18 -0800

    adjusted postgres env vars to use Railway's postgres env vars

--- packages/api/src/data-source.ts
+++ packages/api/src/data-source.ts
@@ -12,12 +12,12 @@ import { EZNotification } from './EZNotification/entities/EZNotification.entity'
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432', 10),
+    host: process.env.PGHOST || 'localhost',
+    port: parseInt(process.env.PGPORT || '5432', 10),
     schema: 'public',
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    database: process.env.DB_DATABASE || 'notifications_dev2',
+    username: process.env.PGUSER || 'postgres',
+    password: process.env.PGPASSWORD || 'postgres',
+    database: process.env.PGDATABASE || 'notifications_dev2',
     entities: [EZNotification, EndUser, EndUsersServed, Organization, User, UserEmails, ApiKey, PermittedDomains, PricingModel],
     synchronize: false,
     migrations: ["src/migration/*.ts"],
