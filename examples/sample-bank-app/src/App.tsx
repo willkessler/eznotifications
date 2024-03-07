2b71c859c58046f1928f6da7ef014f37fda616cb
Will Kessler <willkessler@gmail.com>
2024-03-06 21:17:59 -0800

    much improved sample bank app. needs to be responsive

--- examples/sample-bank-app/src/App.tsx
+++ examples/sample-bank-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { MantineProvider, Anchor, AppShell, Burger,Container, Group, Image,  Skeleton, Text,Title } from '@mantine/core';
+import { MantineProvider, Anchor, AppShell, Burger, Button, Container, Group, Image,  Stack, Skeleton, Text,Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import '@mantine/core/styles.css';
 import { HomePage } from './components/HomePage';
@@ -8,11 +8,11 @@ export function App() {
   const [opened, { toggle }] = useDisclosure();
 
   return (
-    <Container fluid style={{width:'100%'}}>
+    <Container fluid className={classes.outerContainer}>
     <AppShell
-      header={{ height: 120 }}
+      header={{ height: 170 }}
       navbar={{
-          width: '150px',
+          width: '200px',
           breakpoint: 'sm',
           collapsed: { mobile: !opened },
       }}
@@ -27,26 +27,29 @@ export function App() {
         />
           <Group justify="flex-start">
           <Image 
-            radius="md"
-            h={100}
-            fit="contain"
-            src="PorfolioIcon.webp" />
-          <Title className={classes.title}>Uncle Al's Savings and Loan</Title>
-</Group>
+            h={150}
+            src="UncleAlsLogo.webp" />
+          <Title className={classes.title}>Uncle Al's<span style={{fontSize:'20px'}}> Neighborhood Credit Union</span></Title>
+         </Group>
      </AppShell.Header>
 
       <AppShell.Navbar className={classes.nav} p="md">
-          <Anchor >Digital Wallet</Anchor>
-          <Anchor >Get Help</Anchor>
-          <Anchor >Logout</Anchor>
+          <Stack justify="flex-start" align="flex-start">
+            <Anchor fw={500} fz="lg" component="button">Transfer Money</Anchor>
+            <Anchor fw={500} fz="lg" component="button">Pay Bills</Anchor>
+            <Anchor fw={500} fz="lg" component="button">Deposit Checks</Anchor>
+            <Anchor fw={500} fz="lg" component="button">View Statements</Anchor>
+            <br />
+            <Anchor fw={500} fz="lg" component="button">Logout</Anchor>
+          </Stack>
       </AppShell.Navbar>
  
-       <AppShell.Main>
+       <AppShell.Main className={classes.main}>
          <HomePage />
        </AppShell.Main>
 
        <AppShell.Footer className={classes.footer}>
-          <Text>Copyright (c) 2024 Uncle Al's Savings and Loan, Inc</Text>
+          <Text>Copyright (c) 2024 Uncle Al's Neighborhood Credit Union, LLC</Text>
        </AppShell.Footer>
 
     </AppShell>
--- examples/sample-bank-app/src/components/HomePage.tsx
+++ examples/sample-bank-app/src/components/HomePage.tsx
@@ -1,35 +1,46 @@
 import React from 'react';
-import { Card, Text, Title, Button, Group } from '@mantine/core';
+import { Card, Image, Text, Title, Button, Group } from '@mantine/core';
 import '@mantine/core/styles.css';
 import classes from './HomePage.module.css'; // Adjust the path as necessary
 
 export function HomePage() {
   return (
-    <div>
+    <div className={classes.homePageOuter}>
         <div className={classes.header}>
-          <Title order={2}>Welcome back, Alfonze!</Title>
+          <Title order={2} className={classes.welcomeMsg}>Welcome back, Alfonze!</Title>
         </div>
         <Group position="left" className={classes.mainContent} >
-          <Card shadow="sm" p="lg" radius="lg">
-            <Text weight={500}>Checking Account</Text>
+          <Card shadow="sm" p="lg" radius="md"  className={classes.card}>
+          <Image 
+            radius="md"
+            h={120}
+            fit="contain"
+            src="CheckingAccountIcon.webp" />
+            <Text className={classes.cardText} weight={500}>Checking Account</Text>
             <Text size="lg" mt="xs">$1,234.56</Text>
           </Card>
-          <Card shadow="sm" p="lg" radius="lg">
-            <Text weight={500}>Savings Account</Text>
+          <Card shadow="sm" p="lg" radius="md" className={classes.card}>
+            <Image 
+            radius="md"
+            h={120}
+            fit="contain"
+            src="SavingsAccountIcon.webp" />
+            <Text className={classes.cardText} weight={500}>Savings Account</Text>
             <Text size="lg" mt="xs">$4,567.89</Text>
           </Card>
-          <Card shadow="sm" p="lg" radius="lg">
-            <Text weight={500}>Investment Portfolio</Text>
+          <Card shadow="sm" p="lg" radius="md" className={classes.card}>
+          <Image 
+            radius="md"
+            h={120}
+            fit="contain"
+            src="PortfolioIcon.webp" />
+            <Text className={classes.cardText} weight={500}>Investment Portfolio</Text>
             <Text size="lg" mt="xs">$8,910.11</Text>
           </Card>
         </Group>
       <div className={classes.controls}>
       <Group position="center" mt="md" >
-          <Button size="lg">Transfer Money</Button>
-          <Button size="lg">Pay Bills</Button>
-          <Button size="lg">Deposit Checks</Button>
-          <Button size="lg">View Statements</Button>
-        </Group>
+      </Group>
       </div>
       </div>
   );
