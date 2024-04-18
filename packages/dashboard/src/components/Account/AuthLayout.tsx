56f1460bde3f1494c970fa86da5b1ffb9279bdc2
Will Kessler <willkessler@gmail.com>
2024-04-18 12:40:31 -0700

    updates to demo login page

--- packages/dashboard/src/components/Account/AuthLayout.tsx
+++ packages/dashboard/src/components/Account/AuthLayout.tsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import classes from './css/AuthLayout.module.css';
-import { Anchor } from '@mantine/core';
 import { IconArrowElbowRight,
          IconEdit,
          IconTrash
        } from '@tabler/icons-react';
-import { Card, Code, Image, Title } from '@mantine/core';
+import { Anchor, Card, Code, Image, Text, Title } from '@mantine/core';
 
 
-const AuthLayout:React.FC<{children: React.ReactNode, imageUrl: string }> = ({children, imageUrl}) => (
-  <div className={classes.authContainer}>
+const AuthLayout:React.FC<{children: React.ReactNode, imageUrl: string }> = ({children, imageUrl}) => {
+ 
+  return (
+    <div className={classes.authContainer}>
     <div className={classes.leftColumn} style={{ backgroundImage: `url(${imageUrl})` }}>
-      {/* This div will have the background image */}
+    {/* This div will have the background image */}
     </div>
     <div className={classes.rightColumn}>
-      <div className={classes.authComponents}>
-        {children} {/* This will render SignIn or SignUp components */}
-      </div>
-      { import.meta.env.VITE_IS_DEMO_SITE === 'true' && (
+    <div className={classes.authComponents}>
+    {children} {/* This will render SignIn or SignUp components */}
+    </div>
+    { import.meta.env.VITE_IS_DEMO_SITE === 'true' && (
+      <>
         <Card style={{marginLeft:'20px'}}>
           <Title style={{marginBottom:'5px',fontStyle:'italic'}} order={5}>Login Info for this demo site:</Title>
-          <Image w={180} src="demoUserPass.png" />
+          <Image  src="demoUserPass2.png" />
         </Card>
-      )}
-      <div className={classes.customerNameplates}>
-        {/* Customer nameplates and icons */}
+        <br />
+        <Title order={6}>If you are seeing this signin page in an iframe, click <Anchor target="_blank" href="/">here</Anchor> to break out before logging in.</Title>
+      </>
+    )}
+    {/*
+        <div className={classes.customerNameplates}>
         Trusted by these brands:
-          <span className={classes.brandLogo}><IconEdit /></span>
-          <span className={classes.brandLogo}><IconTrash /></span>
-      </div>
-      <Anchor href="/help" underline="never" size="md" className={classes.learnMore}>Learn more about what we do!</Anchor>
+        <span className={classes.brandLogo}><IconEdit /></span>
+        <span className={classes.brandLogo}><IconTrash /></span>
+        </div>
+        <Anchor href="/help" underline="never" size="md" className={classes.learnMore}>Learn more about what we do!</Anchor>
+      */}
     </div>
-  </div>
-);
+    </div>
+  );
+};
 
 export default AuthLayout;
