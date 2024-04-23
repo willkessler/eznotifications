7b2c65d41b11834fcb3202fbb5f4ed529cbe3565
Will Kessler <willkessler@gmail.com>
2024-04-23 16:54:37 -0700

    added free messaging to the signup page

--- packages/dashboard/src/components/Account/SignUpComponent.tsx
+++ packages/dashboard/src/components/Account/SignUpComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useUser, SignUp } from "@clerk/clerk-react";
 import AuthLayout from './AuthLayout';
+import { Card, Text, Title } from '@mantine/core';
 
 const SignUpComponent = () => {
   const { isSignedIn } = useUser();
@@ -25,6 +26,10 @@ const SignUpComponent = () => {
           }}
           signInUrl="/login" 
         />
+        <Card style={{marginLeft:'50px', marginRight: '35px', marginTop:'50px'}}>
+          <Title order={4}>Creating an account is free.</Title>
+          <Text style={{marginTop:'10px', color:'#999'}}><span style={{fontStyle:'italic'}}>This Is Not A Drill!</span> is currently in Beta. During this period, there will be no charge for using the service, but we welcome your feedback.</Text>
+        </Card>
       </AuthLayout>
     </>
   );
