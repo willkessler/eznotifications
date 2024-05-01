073119b6090053123958f23abef35fcba72acdfe
Will Kessler <willkessler@gmail.com>
2024-05-01 10:09:21 -0700

    made inline notifs work right and prevent overlay from interfering with any content

--- packages/sdk/vanillajs/src/InlineNotifications.ts
+++ packages/sdk/vanillajs/src/InlineNotifications.ts
@@ -3,18 +3,25 @@ import './css/inlineNotifications.css';
 export class InlineNotification {
   private containers: HTMLElement[];  // Store references to all containers
   private dismissCallback: (notificationUuid: string) => Promise<void>;
-  private notificationUuid: string;
+  private currentNotificationUuid: string;
+  private inlineNotifOn:boolean;
 
   constructor(dismissCallback: (notificationUuid:string) => Promise<void>) {
     this.containers = Array.from(document.querySelectorAll('.tinad-inline'));
     this.dismissCallback = dismissCallback;
+    this.inlineNotifOn = false;
   }
 
-  private clearContainers() {
+  private async clearContainers() {
     this.containers.forEach(container => {
       container.innerHTML = '';
     });
-    this.dismissCallback(this.notificationUuid).catch(console.error);
+    try {
+      await this.dismissCallback(this.currentNotificationUuid);
+      this.inlineNotifOn = false;
+    } catch(error) {
+      console.log(`inlineNotification dismiss error: ${error}`);
+    }
   }
 
   private createContentDiv(content: string, container: HTMLElement): HTMLElement {
@@ -28,7 +35,7 @@ export class InlineNotification {
     const dismissButtonX = document.createElement('button');
     dismissButtonX.className = 'dismiss-x';
     dismissButtonX.textContent = 'x';
-    dismissButtonX.onclick = () => this.clearContainers();
+    dismissButtonX.onclick = async () => { await this.clearContainers(); };
     notificationDiv.appendChild(dismissButtonX);
 
     const okButton = document.createElement('button');
@@ -41,12 +48,16 @@ export class InlineNotification {
   }
 
   show(content: string, notificationUuid: string) {
+    if (this.inlineNotifOn) {
+      return;
+    }
     // Fill all containers with the same content
-    this.notificationUuid = notificationUuid;
+    this.currentNotificationUuid = notificationUuid;
     this.containers.forEach(container => {
       container.innerHTML = '';  // Clear previous content
       container.appendChild(this.createContentDiv(content, container));
     });
+    this.inlineNotifOn = true;
   }
 
 }
