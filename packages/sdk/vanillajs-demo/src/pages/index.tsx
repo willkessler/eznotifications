2fcc7f15275c13c47d89182f6511c3d6e915d2cb
Will Kessler <willkessler@gmail.com>
2024-05-23 17:03:43 -0700

    added logrocket tracking to vanillajs-demo

--- packages/sdk/vanillajs-demo/src/pages/index.tsx
+++ packages/sdk/vanillajs-demo/src/pages/index.tsx
@@ -4,10 +4,27 @@ import Configurator from './configurator';
 import TabbedEditor from './TabbedEditor';
 import { useSdkConfiguration } from '../lib/configuratorContext';
 import './css/widgets.css';
+import LogRocket from 'logrocket';
 
 export default function Home() {
 
   const { setBankIframeIsReadyState } = useSdkConfiguration();
+
+  useEffect(() => {
+    const isLocalhost = ():boolean => {
+      return window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    };
+
+    // Initialize LogRocket for tracking all user sessions except local dev
+    if (!isLocalhost()) {
+      if (process.env.NEXT_PUBLIC_TINAD_LOGROCKET_API_KEY !== undefined) {
+        LogRocket.init(process.env.NEXT_PUBLIC_TINAD_LOGROCKET_API_KEY);
+      }
+      if (process.env.NEXT_PUBLIC_TINAD_CLERK_DEMO_USER_ID !== undefined) {
+        LogRocket.identify(process.env.NEXT_PUBLIC_TINAD_CLERK_DEMO_USER_ID);
+      }
+    }
+  }, []);
 
   useEffect(() => {
     // Listen for a tinad-iframe-ready message from the bank iframe, and when we get it process all queued postmessages from configurator.
