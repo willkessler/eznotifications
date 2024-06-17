38fc20b8363879a8dbc609d67d2543e0cbc4c19c
Will Kessler <willkessler@gmail.com>
2024-06-17 14:36:00 -0700

    added google analytics tag to vanillajs demo app pages

--- packages/sdk/vanillajs-demo/src/pages/_document.tsx
+++ packages/sdk/vanillajs-demo/src/pages/_document.tsx
@@ -6,6 +6,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-YQ2G1QN26V"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YQ2G1QN26V');
+          </script>
         </Head>
         <body>
           <Main />
